3b7557bd36597f3e46fe517e7585c924ed2b6efc
Javi970 <javierpaivag@gmail.com>
2023-06-02 13:25:28 -0300

    add navigation

---
+++ app/services/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ServicePage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem a atque
+        totam dolores numquam perferendis dignissimos laborum ipsa perspiciatis
+        quasi ab soluta voluptas sed dolorum recusandae corporis similique,
+        ratione error vitae? Voluptatibus architecto iure repudiandae neque
+        aliquid quis aliquam voluptates nisi, a dolorum sequi eos fugit natus
+        numquam facilis quibusdam nam quaerat. Soluta porro error vel laborum
+        harum deleniti a autem. Deleniti a impedit magnam officia modi illo
+        dolores maxime accusamus perferendis. Eius, nisi impedit. Accusamus
+        adipisci, nobis enim laborum, unde accusantium esse ducimus libero
+        officia ex commodi deleniti illo sequi quasi, nisi odit in ipsam eius
+        itaque id error.
+      </p>
+    </div>
+  );
+}
+
+export default ServicePage;
