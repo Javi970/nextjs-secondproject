601160529bd0769cc1ffb0a070eb8e2847620e19
Javi970 <javierpaivag@gmail.com>
2023-06-02 13:37:21 -0300

    fetch users

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function IndexPage() {
-  return <div>IndexPage</div>;
+async function fetchUsers() {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data;
+}
+
+async function IndexPage() {
+  const users = await fetchUsers();
+  console.log(users);
+  return (
+    <div>
+      <h1>IndexPage</h1>
+      <div>{JSON.stringify(users)}</div>
+    </div>
+  );
 }
 
 export default IndexPage;
