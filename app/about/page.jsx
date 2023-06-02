8bcf6926caf774bdf8db891ea6daf8b2cdb82f72
Javi970 <javierpaivag@gmail.com>
2023-06-02 11:42:31 -0300

    initial commit

---
+++ app/about/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi maiores
+        explicabo dolore omnis, sit officia repellat quidem qui, beatae sed unde
+        ipsam eaque numquam, nesciunt nostrum quia ipsum possimus facere
+        consequatur culpa nisi quod porro? Quod voluptas id minus similique amet
+        nobis, consequatur accusantium dolore assumenda temporibus voluptates
+        placeat deleniti? Maiores molestiae esse voluptatum excepturi quae ipsa
+        voluptates laboriosam in, ut adipisci a nam totam quasi! Perferendis
+        voluptatibus aspernatur ut nulla obcaecati voluptatem tenetur, quae sed
+        at quibusdam quas dolorum saepe odit rerum. Molestias explicabo sequi,
+        doloremque, voluptates esse error earum dicta laborum dolores quas,
+        maiores minima veniam? Vitae, facilis?
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
